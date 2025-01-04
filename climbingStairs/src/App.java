8202ea4e7ae5c890189505d980116d64c5086425
Devon Burton <devon.burton@live.com>
2025-01-03 23:57:15 -0500

    added an array to store restult values so the function runs at higher valuess without taking exponentially longer to run

--- climbingStairs/src/App.java
+++ climbingStairs/src/App.java
@@ -1,7 +1,22 @@
 class App {
+    int[] values = new int[46];
     public int climbStairs(int n) {
+        int returnValue = 0;
         if(n == 1) return 1;
         if(n == 2) return 2;
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        if(values[n - 1] != 0) returnValue += values[n - 1];
+        else returnValue += climbStairs(n - 1);
+        if(values[n - 2] != 0) returnValue += values[n - 2];
+        else returnValue += climbStairs(n - 2);
+        values[n] = returnValue;
+        return returnValue;
     }
 }
+
+// class App {
+//     public int climbStairs(int n) {
+//         if(n == 1) return 1;
+//         if(n == 2) return 2;
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// }
