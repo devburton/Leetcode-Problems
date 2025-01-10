a297a4da3663a6b54c8091ce51e9d843a446b284
Devon Burton <devon.burton@live.com>
2025-01-10 04:55:46 -0500

    implemented a new approach to reccursively find values still O(n) but faster

--- CountingBits/src/App.java
+++ CountingBits/src/App.java
@@ -2,47 +2,88 @@ public class App {
     public static void main(String[] args) throws Exception {
         int input = 4;
         int[] output = countBits(input);
-        for(int n : output)  {
+        for (int n : output) {
             System.out.println(n);
         }
     }
 
     public static int[] countBits(int n) {
-        int sig = 1;
         int[] dp = new int[n + 1];
         for(int i = 1; i <= n; i++) {
-            if(i == sig * 2) sig = i;
-            dp[i] = 1 + dp[i - sig];
+            dp[i] = dp[i >> 1] + (i & 1);
         }
         return dp;
     }
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public static void main(String[] args) throws Exception {
+// int input = 4;
+// int[] output = countBits(input);
+// for(int n : output) {
+// System.out.println(n);
+// }
+// }
+
+// public static int[] countBits(int n) {
+// int sig = 1;
+// int[] dp = new int[n + 1];
+// for(int i = 1; i <= n; i++) {
+// if(i == sig * 2) sig = i;
+// dp[i] = 1 + dp[i - sig];
+// }
+// return dp;
+// }
+// }
+
+// inital solution runs O(nlog(n))
 // public class App {
-//     public static void main(String[] args) throws Exception {
-//         int input = 4;
-//         int[] output = countBits(input);
-//         for(int n : output)  {
-//             System.out.println(n);
-//         }
-//     }
+// public static void main(String[] args) throws Exception {
+// int input = 4;
+// int[] output = countBits(input);
+// for(int n : output) {
+// System.out.println(n);
+// }
+// }
 
-//     public static int[] countBits(int n) {
-        
-//         int[] output = new int[n + 1];
-//         for(int i = 0; i <= n; i++) {
-//             output[i] = countBinary1s(i);
-//         }
-//         return output;
-//     }
+// public static int[] countBits(int n) {
 
-//     public static int countBinary1s(int n) {
-//         int ones = 0;
-//         while(n > 0) {
-//             if(n % 2 == 1) ones += 1; 
-//             n /= 2;
-//         }
-//         return ones;
-//     }
+// int[] output = new int[n + 1];
+// for(int i = 0; i <= n; i++) {
+// output[i] = countBinary1s(i);
+// }
+// return output;
+// }
+
+// public static int countBinary1s(int n) {
+// int ones = 0;
+// while(n > 0) {
+// if(n % 2 == 1) ones += 1;
+// n /= 2;
+// }
+// return ones;
+// }
 // }
