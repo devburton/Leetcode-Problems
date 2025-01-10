f2a19100e2b50db010063e2fac1044eef698b3cc
Devon Burton <devon.burton@live.com>
2025-01-09 23:46:48 -0500

    original solution for counting bits problem

---
+++ CountingBits/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int input = 4;
+        int[] output = countBits(input);
+        for(int n : output)  {
+            System.out.println(n);
+        }
+    }
+
+    public static int[] countBits(int n) {
+        
+        int[] output = new int[n + 1];
+        for(int i = 0; i <= n; i++) {
+            output[i] = countBinary1s(i);
+        }
+        return output;
+    }
+
+    public static int countBinary1s(int n) {
+        int ones = 0;
+        while(n > 0) {
+            if(n % 2 == 1) ones += 1; 
+            n /= 2;
+        }
+        return ones;
+    }
+}
