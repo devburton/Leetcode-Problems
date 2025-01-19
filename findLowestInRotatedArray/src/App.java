27ae6a89bb57a4533b120c75be14961afb0b62da
Devon Burton <devon.burton@live.com>
2025-01-18 20:11:14 -0500

    implemented binary search alg

--- findLowestInRotatedArray/src/App.java
+++ findLowestInRotatedArray/src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        int[] input = {3,4,5,1,2};
+        System.out.println(findMin(input));
+    }
+
+    public static int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        int m = (int) (0.5 * (l + r));
+        if(nums[r] > nums[l]) return nums[l];
+        while(l != r) {
+            if(nums[m] >= nums[l]) {
+                l = m;
+                m = (int) (0.5 * (l + r));
+            } else {
+                r = m;
+                m = (int) (0.5 * (l + r));
+            }
+            if(nums[r] > nums[l]) return nums[l];
+            if(r - l == 1) return nums[r];
+        }
+        return nums[l];
     }
 }
