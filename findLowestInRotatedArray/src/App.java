8ae4a3b41db68dfe8b8c0ac1a2698daef8924b6a
Devon Burton <devon.burton@live.com>
2025-01-19 23:02:24 -0500

    tested a new input array

--- findLowestInRotatedArray/src/App.java
+++ findLowestInRotatedArray/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] input = {3,4,5,1,2};
+        int[] input = {3,4,5,6,1,2};
         System.out.println(findMin(input));
     }
 
